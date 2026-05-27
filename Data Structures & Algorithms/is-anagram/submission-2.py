6ec89164eb17a642c059f0f6f0b6accf25998c71
Darsh Parikh <darshparikh00@gmail.com>
2026-05-27 12:03:29 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Edge case:
+        if len(s) != len(t):
+            return False
+
+        # TC: O(nlogn), SC: O(n)
+        # sortedS = sorted(s)
+        # sortedT = sorted(t)
+
+        # for i in range(len(sortedS)):
+        #     if sortedS[i] != sortedT[i]:
+        #         return False
+
+        # return True
+
+        count = {}
+
+        for i in range(len(s)):
+            count.setdefault(s[i], 0)
+            count[s[i]] += 1
+            count.setdefault(t[i], 0)
+            count[t[i]] -= 1
+
+        for val in count.values():
+            if val != 0:
+                return False
+
+        return True
